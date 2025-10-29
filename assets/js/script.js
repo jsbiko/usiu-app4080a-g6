c834f9eb93503cff8b5e8e0fe93a247ce67baa2f
jsbiko <bikos88@yahoo.com>
2025-10-29 09:36:36 +0300

    Aos ANIMATION resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,15 +81,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
-<nav>
-        <ul class="nav-links">
-          <li><a href="index.html" class="active">Home</a></li>
-          <li><a href="our-work.html">Sample Project</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="our-team.html">Our Team</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-      </nav>
-
